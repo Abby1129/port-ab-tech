2d4d619c936f24d15833f0ac33ce50a498c40aad
Abby F <abby_faigao@yahoo.com>
2023-01-27 01:25:01 -0800

    add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img />
+      </div>
+
+      {/* menu */}
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* hamburger */}
+
+      {/* mobile menu */}
+
+      {/* social icons */}
+    </div>
+  );
+};
+
+export default Navbar;
