e31522398807b765d6d4dd02881bc3466ef6c17d
Abby F <abby_faigao@yahoo.com>
2023-01-28 17:26:57 -0800

    add mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-black text-gray-300">
       <div>
         <img />
       </div>
 
       {/* menu */}
-      <ul>
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -17,8 +18,18 @@ const Navbar = () => {
       </ul>
 
       {/* hamburger */}
+      <div className="md:hidden z-10">
+        <FaBars /> : <FaTimes />
+      </div>
 
       {/* mobile menu */}
+      <ul className="absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
 
       {/* social icons */}
     </div>
