09b91f864d3f3a524c3e3d4699de920cd26803f0
Abby F <abby_faigao@yahoo.com>
2023-01-29 17:28:52 -0800

    added layer base index css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-black text-gray-300">
       <div>
-        <img />
+        <img src={Logo} alt="Logo" style={{ width: "100px" }} />
       </div>
 
       {/* menu */}
@@ -23,7 +24,7 @@ const Navbar = () => {
       </div>
 
       {/* mobile menu */}
-      <ul className="absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+      <ul className=" hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
