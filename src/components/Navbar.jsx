1518790f79c4b7e1e3fe273f7cf9ef13ae8a8d39
Abby F <abby_faigao@yahoo.com>
2023-02-03 11:46:25 -0800

    fix mobile menu to be responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#203A43] text-pink-300 text-lg">
       <div>
@@ -12,23 +16,29 @@ const Navbar = () => {
       </div>
 
       {/* menu */}
-      <ul className="hidden md:flex">
+      <ul className="hidden md:flex ">
         <li>Home</li>
-        <li>About</li>
+        {/* <li>About</li> */}
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
       </ul>
 
       {/* hamburger */}
-      <div className="md:hidden z-10">
-        <FaBars /> : <FaTimes />
+      <div className="md:hidden z-10" onClick={handleClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* mobile menu */}
-      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#203A43] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
+        {/* <li className="py-6 text-4xl">About</li> */}
         <li className="py-6 text-4xl">Skills</li>
         <li className="py-6 text-4xl">Work</li>
         <li className="py-6 text-4xl">Contact</li>
