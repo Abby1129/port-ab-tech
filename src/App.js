2a69765a814bbb880c197c928e1b7ed59a38a958
Abby F <abby_faigao@yahoo.com>
2023-02-07 13:37:37 -0800

    skills component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Skills />
     </>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Skills</p>
+          <p>Technologies I've worked with</p>
+        </div>
+
+        <div>
+          <div>
+            <img src="/" alt="/" />
+            <p>HTML</p>
+          </div>
+          <div>
+            <img src="/" alt="/" />
+            <p>HTML</p>
+          </div>
+          <div>
+            <img src="/" alt="/" />
+            <p>HTML</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
