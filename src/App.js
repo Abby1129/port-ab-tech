c28c0896e07e5e2bd9cb1b73733069bc5a298af1
Abby F <abby_faigao@yahoo.com>
2023-02-11 18:25:25 -0800

    update work component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <Skills />
+      <Work />S
     </>
   );
 }
