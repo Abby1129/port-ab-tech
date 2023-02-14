d73ea34c23eb97a539402a3a6a8b6d9f82bcd669
Abby F <abby_faigao@yahoo.com>
2023-02-13 19:41:34 -0800

    contact comp markup

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <form>
+        <div>
+          <p>Contact</p>
+          <p>Submit form below or email me - abbyfaigao@yahoo.com</p>
+        </div>
+        <input />
+        <input />
+        <textarea></textarea>
+        <button>Let's Connect</button>
+      </form>
+    </div>
+  );
+}
